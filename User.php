13b66a4dde290293432140baca734514fdf80589
phucbienvan <78720117+phucbienvan@users.noreply.github.com>
2022-12-20 20:29:57 +0700

    Update User.php

--- User.php
+++ User.php
@@ -6,31 +6,42 @@ use App\Models\User;
 use App\Models\Category;
 use App\Models\Permission;
 
-class UserService {
-    public function __construct(User $user,
-        Category $category, Permission $permission) {
+class UserService
+{
+    public function __construct(
+        User $user,
+        Category $category, 
+        Permission $permission
+    ) {
         $this->user = $user;
         $this->category = $category;
         $this->permission = $permission;
     }
 
-    public function create_user($param)
+    public function createUser($params)
     {
         $user = [
             'name' => $param['name'],
             'email' => $param['email']
         ];
-        if(isset($param['password'] && $param['password'])){
-        	$user['password'] = $param['password']
+        
+        if (isset($param['password'])) {
+        	$user['password'] = bcrypt($param['password']);
         }
+        
         return $this->user->create($user);
     }
 
     public function search($param){
-    	$UserSearch = $this->user;
+    	$userSearch = $this->user;
+        
     	if ($param['keyword']) {
-    		$UserSearch->where('name', "like","%$param['keyword']");
+    		$userSearch->where('name', 'like', '%' . $param['keyword']);
     	}
-    	return $UserSearch->with("role",'group')->where('status', true)->whereHas('schedule')->paginate();
+        
+    	return $userSearch->with("role",'group')
+            ->where('status', true)
+            ->whereHas('schedule')
+            ->paginate(5);
     }
 }
